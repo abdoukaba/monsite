c4184039f1df5e4599318aa12dc379d92bf024ab
abdoukaba <abdoukaba@rocketmail.com>
2014-12-06 03:01:51 +0100

    Update DBFactory.class.php

--- membre/lib/DBFactory.class.php
+++ membre/lib/DBFactory.class.php
@@ -3,12 +3,12 @@
  {  
  public static function getMysqlConnexionWithPDO()
  {  
- $db = new PDO('mysql:host=db555693792.db.1and1.com', 'dbo555693792', 'Swish@5house');  
+ $db = new PDO('mysql:host=db555693792.db.1and1.com', 'dbo555693792', 'password');  
  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     
  return $db; 
  }  
  public static function getMysqlConnexionWithMySQLi() 
  {    
- return new MySQLi('db555693792.db.1and1.com', 'dbo555693792', 'Swish@5house', 'db555693792');  
+ return new MySQLi('db555693792.db.1and1.com', 'dbo555693792', 'password', 'db555693792');  
  } 
  }
